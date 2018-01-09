dba1591c23e5eca0ed2101ad526bc5f5fa100679
Max Heikenfeld <maxheikenfeld@web.de>
2018-01-09 14:31:31 +0000

    added 2D watershedding and removed name of Water content from field since it could basically be any field we perform the watershedding on

--- cloudtrack/watershedding.py
+++ cloudtrack/watershedding.py
@@ -1,14 +1,14 @@
-def watershedding(Track,WC,WC_threshold=3e-3,level=None,compactness=0,method='watershed'):
+def watershedding_3D(Track,Field_in,Field_threshold=3e-3,level=None,compactness=0,method='watershed'):
     """
     Function using watershedding to determine cloud volumes associated with tracked updrafts
     
     Parameters:
     Track:         pandas.DataFrame 
                    output from trackpy/maketrack
-    WC:            iris.cube.Cube 
-                   containing the water content field to use the watershedding on 
-    WC_threshold:  float 
-                   threshold for the watershedding
+    Field_in:      iris.cube.Cube 
+                   containing the field to perform the watershedding on 
+    Field_threshold:  float 
+                   threshold for the watershedding field to be used for the mask
     level          slice
                    levels at which to seed the particles for the watershedding algorithm
     compactness    float
@@ -31,33 +31,101 @@ def watershedding(Track,WC,WC_threshold=3e-3,level=None,compactness=0,method='wa
     if level==None:
         level=slice(None)
     
-    Data=copy.deepcopy(WC)
+    Data=copy.deepcopy(Field_in)
     Data.data[:,:,:]=0
-    Watershed_out=copy.deepcopy(WC)
+    Watershed_out=copy.deepcopy(Field_in)
     Watershed_out.rename('watershedding_output_mask')
     Watershed_out.data[:]=0
     Watershed_out.units=1
-    for i, time in enumerate(WC.coord('time').points):        
+    for i, time in enumerate(Field_in.coord('time').points):        
 #        print('doing watershedding for',WC.coord('time').units.num2date(time).strftime('%Y-%m-%d %H:%M:%S'))
         Tracks_i=Track[Track['frame']==i]
-        data_i=WC[i,:].data        
+        data_i=Field_in[i,:].data        
         Mask= np.zeros_like(data_i).astype(np.int16)
-        Cloudy=data_i>WC_threshold
-        Mask[Cloudy]=1
-        markers = np.zeros_like(Cloudy).astype(np.int16)
+        unmasked=data_i>Field_threshold
+        Mask[unmasked]=1
+        markers = np.zeros_like(unmasked).astype(np.int16)
         for index, row in Tracks_i.iterrows():
             markers[:,round(row.y), round(row.x)]=row.particle
-        data_i[~Cloudy]=0
-        markers[~Cloudy]=0
+        data_i[~unmasked]=0
+        markers[~unmasked]=0
 
         data_i_watershed=(1-data_i*10)*1000
-        data_i_watershed[~Cloudy]=2000
+        data_i_watershed[~unmasked]=2000
 
         data_i_watershed=data_i_watershed.astype(np.uint16)
         #res1 = watershed_ift(data_i_watershed, markers)
         
         if method=='watershed':
-            res1 = watershed(data_i_watershed,markers.astype(np.int8), mask=Cloudy,compactness=compactness)
+            res1 = watershed(data_i_watershed,markers.astype(np.int8), mask=unmasked,compactness=compactness)
+        elif method=='random_walker':
+            #res1 = random_walker(Mask, markers,mode='cg')
+             res1=random_walker(data_i_watershed, markers.astype(np.int8), beta=130, mode='bf', tol=0.001, copy=True, multichannel=False, return_full_prob=False, spacing=None)
+        else:
+            print('unknown method')
+        Watershed_out.data[i,:]=res1
+    return Watershed_out
+
+def watershedding_2D(Track,Field_in,Field_threshold=0,level=None,compactness=0,method='watershed'):
+    """
+    Function using watershedding to determine cloud volumes associated with tracked updrafts
+    
+    Parameters:
+    Track:         pandas.DataFrame 
+                   output from trackpy/maketrack
+    Field_in:      iris.cube.Cube 
+                   containing the field to perform the watershedding on 
+    Field_threshold:  float 
+                   threshold for the watershedding field to be used for the mask
+    level          slice
+                   levels at which to seed the particles for the watershedding algorithm
+    compactness    float
+                   parameter describing the compactness of the resulting volume
+    
+    Output:
+    Watershed_out: iris.cube.Cube
+                   Cloud mask, 0 outside and integer numbers according to track inside the clouds
+    
+    """
+    
+    import numpy as np
+    import copy
+    from skimage.morphology import watershed
+    from skimage.segmentation import random_walker
+#    from scipy.ndimage.measurements import watershed_ift
+
+    #Set level at which to create "Seed" for each cloud and threshold in total water content:
+    # If none, use all levels (later reduced to the ones fulfilling the theshold conditions)
+    if level==None:
+        level=slice(None)
+    
+    Data=copy.deepcopy(Field_in)
+    Data.data[:,:]=0
+    Watershed_out=copy.deepcopy(Field_in)
+    Watershed_out.rename('watershedding_output_mask')
+    Watershed_out.data[:]=0
+    Watershed_out.units=1
+    for i, time in enumerate(Field_in.coord('time').points):        
+#        print('doing watershedding for',WC.coord('time').units.num2date(time).strftime('%Y-%m-%d %H:%M:%S'))
+        Tracks_i=Track[Track['frame']==i]
+        data_i=Field_in[i,:].data        
+        Mask= np.zeros_like(data_i).astype(np.int16)
+        unmasked=data_i>Field_threshold
+        Mask[unmasked]=1
+        markers = np.zeros_like(unmasked).astype(np.int16)
+        for index, row in Tracks_i.iterrows():
+            markers[round(row.y), round(row.x)]=row.particle
+        data_i[~unmasked]=0
+        markers[~unmasked]=0
+
+        data_i_watershed=(1-data_i*10)*1000
+        data_i_watershed[~unmasked]=2000
+
+        data_i_watershed=data_i_watershed.astype(np.uint16)
+        #res1 = watershed_ift(data_i_watershed, markers)
+        
+        if method=='watershed':
+            res1 = watershed(data_i_watershed,markers.astype(np.int8), mask=unmasked,compactness=compactness)
         elif method=='random_walker':
             #res1 = random_walker(Mask, markers,mode='cg')
              res1=random_walker(data_i_watershed, markers.astype(np.int8), beta=130, mode='bf', tol=0.001, copy=True, multichannel=False, return_full_prob=False, spacing=None)
