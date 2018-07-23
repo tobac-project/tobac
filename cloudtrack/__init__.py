2d3d80aed3a9dfd0285ddedf1575312be6d66b25
Max Heikenfeld <maxheikenfeld@web.de>
2018-07-23 16:37:35 +0100

    add lifetime histogram calculations and plotting to __init__.py

--- cloudtrack/__init__.py
+++ cloudtrack/__init__.py
@@ -2,4 +2,5 @@ from .tracking import maketrack
 from .watershedding import watershedding_3D,watershedding_2D,mask_particle,mask_particle_surface,mask_cube_particle,mask_cube_untracked,mask_cube
 from .centerofgravity import calculate_cog,calculate_cog_untracked,calculate_cog_domain
 from .plotting import plot_tracks_mask_field,plot_tracks_mask_field_loop,plot_mask_cell_track_follow,plot_mask_cell_track_static
-from .analysis import cell_statistics,cog_cell
+from .plotting import plot_lifetime_histogram,plot_lifetime_histogram_bar
+from .analysis import cell_statistics,cog_cell,lifetime_histogram
