542a9309537fbe1d35f047413134ce6d08a96776
Max Heikenfeld <maxheikenfeld@web.de>
2018-09-27 15:21:52 +0100

    added functions to __init__.py (probably not complete)

--- cloudtrack/__init__.py
+++ cloudtrack/__init__.py
@@ -5,7 +5,7 @@ from .plotting import plot_tracks_mask_field,plot_tracks_mask_field_loop,plot_ma
 from .plotting import plot_lifetime_histogram,plot_lifetime_histogram_bar,plot_histogram_cellwise,plot_histogram_featurewise
 from .analysis import cell_statistics,cog_cell,lifetime_histogram,histogram_featurewise,histogram_cellwise
 from .utils import mask_cell,mask_cell_surface,mask_cube_cell,mask_cube_untracked,mask_cube,column_mask_from2D,get_bounding_box
-
+from .utils import mask_features,mask_features_surface,mask_cube_features
 
 from .utils import add_coordinates,get_spacings
 from .feature_detection import feature_detection_threshold,feature_detection_multithreshold
