dd0ccfa8cfb06faaef4c0224abdcbfbefb1a0147
Max Heikenfeld <maxheikenfeld@web.de>
2020-03-29 22:26:38 +0200

    added versioning based on github tag to setup.py, fixed license there (#27)

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='tobac',
-      version='1.2',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
       description='Tracking and object-based analysis of clouds',
       url='http://github.com/climate-processes/tobac',
       author='Max Heikenfeld',
-      author_email='max.heikenfeld@physics.ox.ac.uk',
-      license='GNU',
+      author_email='max.heikenfeld@web.de',
+      license='BSD-3',
       packages=['tobac','tobac.analysis','tobac.plot','tobac.themes','tobac.themes.tobac_v1'],
       install_requires=[],
       zip_safe=False)
