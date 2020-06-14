b71f774e32fe7cf25db83a522a32f57642eb238c
Max Heikenfeld <maxheikenfeld@web.de>
2020-06-14 23:36:20 +0200

    cleaned up __init__.py for tobac_v1 theme

--- tobac/themes/tobac_v1/__init__.py
+++ tobac/themes/tobac_v1/__init__.py
@@ -1,4 +1,4 @@
-from .mask_segmentation import segmentation, segmentation_2D, segmentation_3D
+from .mask_segmentation import segmentation
 from .feature_detection import feature_detection_multithreshold
 from .tracking import linking_trackpy
 
