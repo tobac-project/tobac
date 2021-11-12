7685da16816e81277196c8d4f8d4f5c48962c6a8
Sean Freeman <sean.freeman@colostate.edu>
2021-11-12 09:02:48 -0700

    Added conf.py for sphinx to the doc folder.

---
+++ doc/conf.py
@@ -0,0 +1,19 @@
+import sphinx_rtd_theme
+import sys, os
+
+sys.path.insert(0, os.path.abspath('extensions'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig',
+              'sphinx_rtd_theme',]
+
+
+html_theme = "sphinx_rtd_theme"
+
+project = u'tobac'
+
+
+def setup(app):
+   app.add_css_file("theme_overrides.css")
+
+
