602936253b6b6ea4c0e2bf84b200a5fa060c31a8
galexsky <90701223+galexsky@users.noreply.github.com>
2022-01-10 11:52:52 -0700

    Updated version number and author list
    Version number updated to 1.3.1; Sean Freeman and Alex Sokolowsky added as authors

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='tobac',
-      version='1.2',
+      version='1.3.1',
       description='Tracking and object-based analysis of clouds',
       url='http://github.com/climate-processes/tobac',
-      author='Max Heikenfeld',
+      author='Max Heikenfeld, Sean Freeman, and Alex Sokolowsky',
       author_email='max.heikenfeld@physics.ox.ac.uk',
       license='GNU',
       packages=['tobac'],
