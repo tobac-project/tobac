057bad4da7504f4b8fef42e994eeb9dca93b71f8
Sean Freeman <sean.freeman@colostate.edu>
2022-01-14 09:47:12 -0700

    Revert "Updated version number and author list"
    This reverts commit 602936253b6b6ea4c0e2bf84b200a5fa060c31a8.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='tobac',
-      version='1.3.1',
+      version='1.2',
       description='Tracking and object-based analysis of clouds',
       url='http://github.com/climate-processes/tobac',
-      author='Max Heikenfeld, Sean Freeman, and Alex Sokolowsky',
+      author='Max Heikenfeld',
       author_email='max.heikenfeld@physics.ox.ac.uk',
       license='GNU',
       packages=['tobac'],
