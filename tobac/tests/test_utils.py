34795888560dce82d2d723ad0fd5384b93635236
Juli <julia.kukulies@gu.se>
2022-06-08 15:16:12 +0200

    add better description of assert statements

--- tobac/tests/test_utils.py
+++ tobac/tests/test_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import tobac.utils as tb_utils
 
 
-
 def test_spectral_filtering():
     """Testing tobac.utils.spectral_filtering with random test data."""
 
@@ -16,10 +15,11 @@ def test_spectral_filtering():
     # use spectral filtering function on random data
     transfer_function, filtered_data = tb_utils.spectral_filtering(dxy, random_data, lambda_min, lambda_max, return_transfer_function = True)
 
-    # a few checks on the output 
-    # wavelength space
+    # a few checks on the output
     wavelengths = transfer_function[0]
+    # first element in wavelengths-space is inf because normalized wavelengths are 0 here
     assert wavelengths[0,0] == np.inf
+    # the first elements should correspond to twice the distance of the corresponding axis (in km) 
     assert wavelengths[1,0] == (dxy/1000) *random_data.shape[-2]*2
     assert wavelengths[0,1] == (dxy/1000) *random_data.shape[-1]*2
 
