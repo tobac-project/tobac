cc903de1fc143bcefa40570e4a51382978b89740
Sean Freeman <freemansw1@gmail.com>
2022-09-12 09:53:13 -0500

    Revisions based on Julia's comments

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ This code is from the python documentation and is
 designed to read in the version number.
 See: https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
 """
-import codecs
-import os.path
+from pathlib import Path
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+def read(pkg_name):
+    init_fname = Path(__file__).parent / pkg_name / "__init__.py"
+    with open(init_fname, "r") as fp:
         return fp.read()
 
 
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
+def get_version(pkg_name):
+    for line in read(pkg_name).splitlines():
         if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
@@ -25,8 +24,10 @@ def get_version(rel_path):
         raise RuntimeError("Unable to find version string.")
 
 
+PACKAGE_NAME = "tobac"
+
 setup(
-    name="tobac",
+    name=PACKAGE_NAME,
     version=get_version("tobac/__init__.py"),
     description="Tracking and object-based analysis of clouds",
     url="http://github.com/tobac-project/tobac",
@@ -47,7 +48,7 @@ setup(
         "peter.marinescu@colostate.edu",
     ],
     license="BSD-3-Clause License",
-    packages=["tobac"],
+    packages=[PACKAGE_NAME],
     install_requires=[],
     zip_safe=False,
 )
