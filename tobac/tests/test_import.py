a7b96b9aa9f94d320fc7ce3fee1822d1b94ea223
Sean Freeman <sean.freeman@colostate.edu>
2022-10-10 07:43:02 -0600

    Updated version number test

--- tobac/tests/test_import.py
+++ tobac/tests/test_import.py
@@ -8,6 +8,14 @@ def test_dummy_function():
 
 def test_version():
     """Test to make sure that we have a version number included.
+    Also test to make sure that the version number complies with 
+    semantic versioning guidelines.
     If it's not, this should result in an error.
     """
-    assert tobac.__version__ == tobac.__version__
+    import re
+    assert type(tobac.__version__) is str
+    # Make sure that we are following semantic versioning
+    # i.e., our version is of form x.x.x, where x are all 
+    # integer numbers.
+    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+", tobac.__version__) is not None
+
