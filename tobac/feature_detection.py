d0ec74db36b7b913f90d3d01b38c04142b7a280a
Juli <julia.kukulies@gu.se>
2022-10-21 14:30:44 +0200

    add dxy, min_distance, wavelengthfiltering to docstring

--- tobac/feature_detection.py
+++ tobac/feature_detection.py
@@ -454,6 +454,13 @@ def feature_detection_multithreshold_timestep(
     feature_number_start : int, optional
         Feature id to start with. Default is 1.
 
+    dxy : float
+        Grid spacing in meter.
+
+    wavelength_filtering: tuple, optional
+       Minimum and maximum wavelength for spectral filtering in meter. Default is None.
+
+
     Returns
     -------
     features_threshold : pandas DataFrame
@@ -581,10 +588,13 @@ def feature_detection_multithreshold(
         Minimum number of identified features. Default is 0.
 
     min_distance : float, optional
-        Minimum distance between detected features. Default is 0.
+        Minimum distance between detected features in meter. Default is 0.
 
     feature_number_start : int, optional
         Feature id to start with. Default is 1.
+
+    wavelength_filtering: tuple, optional
+       Minimum and maximum wavelength for spectral filtering in meter. Default is None.
 
     Returns
     -------
