1723e4b761fb0aaeb4bf976ce20a8e17b383b37b
Sean Freeman <freemansw1@gmail.com>
2022-10-23 21:39:25 -0500

    added utils to setup modules

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         "peter.marinescu@colostate.edu",
     ],
     license="BSD-3-Clause License",
-    packages=[PACKAGE_NAME],
+    packages=[PACKAGE_NAME, PACKAGE_NAME + ".utils"],
     install_requires=[],
     zip_safe=False,
 )
