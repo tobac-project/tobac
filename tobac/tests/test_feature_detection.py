97118cdd96ed2f0e82f61d18bab51b4494c289d8
Fabian Senf <senf@tropos.de>
2023-05-07 12:25:28 +0200

    added data type testing for function `feature_detection_multithreshold`

--- tobac/tests/test_feature_detection.py
+++ tobac/tests/test_feature_detection.py
@@ -375,17 +375,26 @@ def test_filter_min_distance(
 
 @pytest.mark.parametrize(
     "test_dset_size, vertical_axis_num, "
-    "vertical_coord_name,"
-    " vertical_coord_opt, expected_raise",
+    "vertical_coord_name, "
+    "vertical_coord_opt, expected_raise, "
+    "data_type",
     [
-        ((1, 20, 30, 40), 1, "altitude", "auto", False),
-        ((1, 20, 30, 40), 2, "altitude", "auto", False),
-        ((1, 20, 30, 40), 3, "altitude", "auto", False),
-        ((1, 20, 30, 40), 1, "air_pressure", "air_pressure", False),
-        ((1, 20, 30, 40), 1, "air_pressure", "auto", True),
-        ((1, 20, 30, 40), 1, "model_level_number", "auto", False),
-        ((1, 20, 30, 40), 1, "altitude", "auto", False),
-        ((1, 20, 30, 40), 1, "geopotential_height", "auto", False),
+        ((1, 20, 30, 40), 1, "altitude", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 2, "altitude", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 3, "altitude", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 1, "air_pressure", "air_pressure", False, 'iris'),
+        ((1, 20, 30, 40), 1, "air_pressure", "auto", True, 'iris'),
+        ((1, 20, 30, 40), 1, "model_level_number", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 1, "altitude", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 1, "geopotential_height", "auto", False, 'iris'),
+        ((1, 20, 30, 40), 1, "altitude", "auto", False, 'xarray'),
+        ((1, 20, 30, 40), 2, "altitude", "auto", False, 'xarray'),
+        ((1, 20, 30, 40), 3, "altitude", "auto", False, 'xarray'),
+        ((1, 20, 30, 40), 1, "air_pressure", "air_pressure", False, 'xarray'),
+        ((1, 20, 30, 40), 1, "air_pressure", "auto", True, 'xarray'),
+        ((1, 20, 30, 40), 1, "model_level_number", "auto", False, 'xarray'),
+        ((1, 20, 30, 40), 1, "altitude", "auto", False, 'xarray'),
+        ((1, 20, 30, 40), 1, "geopotential_height", "auto", False, 'xarray'),
     ],
 )
 def test_feature_detection_multiple_z_coords(
@@ -394,6 +403,7 @@ def test_feature_detection_multiple_z_coords(
     vertical_coord_name,
     vertical_coord_opt,
     expected_raise,
+    data_type
 ):
     """Tests ```tobac.feature_detection.feature_detection_multithreshold```
     with different axes
@@ -422,7 +432,7 @@ def test_feature_detection_multiple_z_coords(
     test_data[0, 0:5, 0:5, 0:5] = 3
     common_dset_opts = {
         "in_arr": test_data,
-        "data_type": "iris",
+        "data_type": data_type,
         "z_dim_name": vertical_coord_name,
     }
     if vertical_axis_num == 1:
