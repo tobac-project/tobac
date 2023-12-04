4a8f049d985b939a6db3581a996658ed80c1b3f5
William Jones <william.jones@physics.ox.ac.uk>
2023-12-04 18:30:33 +0000

    Add test for calculate_area with 1D lat/lon coords

--- tobac/tests/test_analysis_spatial.py
+++ tobac/tests/test_analysis_spatial.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import xarray as xr
+from iris.analysis.cartography import area_weights
 
 from tobac.analysis.spatial import calculate_area
 
@@ -114,6 +115,67 @@ def test_calculate_area():
     )
 
     expected_areas = np.array([3, 2, 2])
+
+    area = calculate_area(test_features, test_cube)
+
+    assert np.all(area["area"] == expected_areas)
+
+
+def test_calculate_area_1D_latlon():
+    """
+    Test area calculation using 1D lat/lon coords
+    """
+    test_labels = np.array(
+        [
+            [
+                [0, 0, 0, 0, 0],
+                [0, 1, 0, 2, 0],
+                [0, 1, 0, 2, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+            ],
+        ],
+        dtype=int,
+    )
+
+    test_labels = xr.DataArray(
+        test_labels,
+        dims=("time", "latitude", "longitude"),
+        coords={
+            "time": [datetime(2000, 1, 1)],
+            "latitude": xr.DataArray(
+                np.arange(5), dims=("latitude",), attrs={"units": "degrees"}
+            ),
+            "longitude": xr.DataArray(
+                np.arange(5), dims=("longitude",), attrs={"units": "degrees"}
+            ),
+        },
+    )
+
+    # We need to do this to avoid round trip bug with xarray to iris conversion
+    test_cube = test_labels.to_iris()
+    test_cube = test_cube.copy(test_cube.core_data().filled())
+
+    test_features = pd.DataFrame(
+        {
+            "feature": [1, 2],
+            "frame": [0, 0],
+            "time": [
+                datetime(2000, 1, 1),
+                datetime(2000, 1, 1),
+            ],
+        }
+    )
+
+    # Calculate expected areas
+    copy_of_test_cube = test_cube.copy()
+    copy_of_test_cube.coord("latitude").guess_bounds()
+    copy_of_test_cube.coord("longitude").guess_bounds()
+    area_array = area_weights(copy_of_test_cube, normalize=False)
+
+    expected_areas = np.array(
+        [np.sum(area_array[test_labels.data == i]) for i in [1, 2]]
+    )
 
     area = calculate_area(test_features, test_cube)
 
