de7f8ceac350e199e202e4affbd70f279b856f1f
William Jones <william.jones@physics.ox.ac.uk>
2025-04-29 15:25:14 +0200

    Formatting

--- tobac/segmentation/watershed_segmentation.py
+++ tobac/segmentation/watershed_segmentation.py
@@ -1253,7 +1253,9 @@ def segmentation(
     # create our output dataarray
     segmentation_out_data = (
         xr.zeros_like(field, dtype=int)
-        .drop_attrs(deep=False) # Drop variable attrs (e.g. units, standard name) but not coordinate attrs
+        .drop_attrs(
+            deep=False
+        )  # Drop variable attrs (e.g. units, standard name) but not coordinate attrs
         .rename("segmentation_mask")
         .assign_attrs(threshold=threshold)
     )
