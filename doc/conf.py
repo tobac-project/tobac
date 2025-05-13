39867e068bf6910ae150f87333f87f4d95470542
Sven-248 <starzer.sven@gmail.com>
2025-05-13 16:06:36 +0200

    Add new notebook and update sphinx static files; minor code adjustments in Idealized-Case-3

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,5 @@
 """This file is used to configure the Sphinx build of our documentation.
-The documentation on setting this up is here: https://www.sphinx-doc.org/en/master/usage/configuration.html 
+The documentation on setting this up is here: https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
 # This is the standard readthedocs theme.
@@ -87,6 +87,7 @@ napoleon_attr_annotations = True
 nbsphinx_thumbnails = {
     "examples/Basics/Idealized-Case-1_Tracking-of-a-Test-Blob-in-2D": "_static/thumbnails/Basics_Idealized-Case-1_Tracking-of-a-Test-Blob-in-2D_Thumbnail.png",
     "examples/Basics/Idealized-Case-2_Two_crossing_Blobs": "_static/thumbnails/Basics_Idealized-Case-2_Two_crossing_Blobs_Thumbnail.png",
+    "exs/Doc_Idealized-Case-3_3D-Blob-Tracking": "_static/thumbnails/Basics_Idealized-Case-3_Tracking-of-a-Test-Blob-in-3D_Thumbnail.png",
     "examples/Basics/Methods-and-Parameters-for-Feature-Detection_Part_1": "_static/thumbnails/Basics_Methods-and-Parameters-for-Feature-Detection_Part_1_Thumbnail.png",
     "examples/Basics/Methods-and-Parameters-for-Feature-Detection_Part_2": "_static/thumbnails/Basics_Methods-and-Parameters-for-Feature-Detection_Part_2_Thumbnail.png",
     "examples/Basics/Methods-and-Parameters-for-Linking": "_static/thumbnails/Basics_Methods-and-Parameters-for-Linking_Thumbnail.png",
